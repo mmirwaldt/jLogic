7c67db33dc824608d63d5389d0bf215a83dac1ae
mmirwaldt <IbJg84upP>
2019-04-02 20:03:32 +0200

    javadoc improved

--- PropositionalLogic/src/main/java/net/mirwaldt/logic/propositional/api/Interpretation.java
+++ PropositionalLogic/src/main/java/net/mirwaldt/logic/propositional/api/Interpretation.java
@@ -6,7 +6,7 @@ package net.mirwaldt.logic.propositional.api;
 @FunctionalInterface
 public interface Interpretation {
     /**
-     * get the boolean value (i.e. the interpretation) of the variable
+     * get the boolean value (i.e. the interpretation) for a variable
      * @param variableName the name of the variable
      * @return
      */
