e799e97cc92825552b4465c5a2af4e412d9bb91b
mmirwaldt <IbJg84upP>
2019-04-28 23:12:24 +0200

    truth table replaced by iterator to evaluate a proposition on-the-fly instead preevaluating it

---
+++ PropositionalLogic/src/main/java/net/mirwaldt/logic/propositional/table/impl/LongInterpretationsIterable.java
@@ -0,0 +1,36 @@
+package net.mirwaldt.logic.propositional.table.impl;
+
+import net.mirwaldt.logic.propositional.interpretation.api.Interpretation;
+import net.mirwaldt.logic.propositional.interpretation.impl.LongInterpretation;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static net.mirwaldt.logic.propositional.util.BitUtils.reverseBits;
+
+public class LongInterpretationsIterable implements Iterable<Interpretation> {
+    private final List<String> variableNames;
+
+    public LongInterpretationsIterable(List<String> variableNames) {
+        this.variableNames = variableNames;
+    }
+
+    @Override
+    public Iterator<Interpretation> iterator() {
+        return new Iterator<>() {
+            final int numberOfBits = variableNames.size();
+            final long numberOfRows = 1 << numberOfBits;
+            long rowIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return rowIndex < numberOfRows;
+            }
+
+            @Override
+            public Interpretation next() {
+                return new LongInterpretation(variableNames, reverseBits(rowIndex++, numberOfBits));
+            }
+        };
+    }
+}
--- PropositionalLogic/src/main/java/net/mirwaldt/logic/propositional/table/impl/TruthTableGenerator.java
+++ PropositionalLogic/src/main/java/net/mirwaldt/logic/propositional/table/impl/TruthTableGenerator.java
@@ -2,50 +2,15 @@ package net.mirwaldt.logic.propositional.table.impl;
 
 import net.mirwaldt.logic.propositional.interpretation.api.Interpretation;
 import net.mirwaldt.logic.propositional.proposition.api.Proposition;
-import net.mirwaldt.logic.propositional.interpretation.impl.LongInterpretation;
-import net.mirwaldt.logic.propositional.table.api.TruthTable;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-import static java.util.Comparator.naturalOrder;
-import static net.mirwaldt.logic.propositional.proposition.api.Proposition.*;
-import static net.mirwaldt.logic.propositional.util.BitUtils.encode;
-import static net.mirwaldt.logic.propositional.util.BitUtils.reverseBits;
+import static net.mirwaldt.logic.propositional.proposition.api.Proposition.not;
 import static net.mirwaldt.logic.propositional.util.PropositionUtils.toBit;
 
 public class TruthTableGenerator {
-    public static TruthTable evaluate(Proposition proposition) {
-        final List<String> namesSorted = new ArrayList<>(proposition.findVariableNames());
-        namesSorted.sort(naturalOrder());
-        checkParameters(proposition, namesSorted);
-        
-        final int numberOfBits = namesSorted.size();
-        final long numberOfRows = 1 << numberOfBits;
-        final List<Interpretation> interpretations = new ArrayList<>();
-        long resultsAsBits = 0;
-        for (long rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-            final Interpretation interpretation = new LongInterpretation(namesSorted, reverseBits(rowIndex, numberOfBits));
-            final boolean result = proposition.evaluate(interpretation);
-            if(result) {
-                resultsAsBits = encode(resultsAsBits, rowIndex, 1);
-            }
-            interpretations.add(interpretation);
-        }
-        
-        return new LongTruthTable(interpretations, proposition, resultsAsBits);
-    }
-
-    private static void checkParameters(Proposition proposition, List<String> namesSorted) {
-        if(namesSorted.isEmpty()) {
-            throw new IllegalArgumentException(
-                    "Proposition has no variables! Proposition: '" + proposition.toExpression() + "'");
-        } else if(63 < namesSorted.size()) {
-            throw new IllegalArgumentException(
-                    "More than 63 variables are not supported yet.");
-        }
-    }
-
     public static void main(String[] args) {
         final Proposition A = Proposition.variable("A");
         final Proposition B = Proposition.variable("B");
@@ -53,13 +18,15 @@ public class TruthTableGenerator {
         final Proposition D = Proposition.variable("D");
 
         final Proposition complex = (A.or(B)).nand(not(C).imply(D));
-        TruthTable truthTable = evaluate(complex);
+
+        final List<String> variableNames = new ArrayList<>(complex.findVariableNames());
+        variableNames.sort(Comparator.naturalOrder());
+        final LongInterpretationsIterable longInterpretationsIterable = new LongInterpretationsIterable(variableNames);
+
         System.out.println(
-                String.join(" ", truthTable.getInterpretations().get(0).getVariableNames()) 
-                + " | "+ truthTable.getProposition().toExpression());
-        truthTable.getInterpretations().stream()
-                .map(interpretation -> interpretation.asBitsWhitespaceSeparated() 
-                        + " | " + toBit(truthTable.getResult(interpretation)))
-                .forEach(System.out::println);
+                String.join(" ", variableNames) + " | " + complex.toExpression());
+        for (Interpretation interpretation : longInterpretationsIterable) {
+            System.out.println(interpretation.asBitsWhitespaceSeparated() + " | " + toBit(complex.evaluate(interpretation)));
+        }
     }
 }
