7d3e2b6b6a1167a3ce0f241edda8d4fa934af71b
mmirwaldt <IbJg84upP>
2019-05-01 23:16:42 +0200

    resolution prover improved

--- PropositionalLogic/src/main/java/net/mirwaldt/logic/propositional/prover/impl/ResolutionPropositionProver.java
+++ PropositionalLogic/src/main/java/net/mirwaldt/logic/propositional/prover/impl/ResolutionPropositionProver.java
@@ -6,9 +6,7 @@ import net.mirwaldt.logic.propositional.proposition.impl.NotProposition;
 import net.mirwaldt.logic.propositional.proposition.impl.OrProposition;
 import net.mirwaldt.logic.propositional.prover.api.PropositionProver;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import static net.mirwaldt.logic.propositional.proposition.api.Proposition.not;
 import static net.mirwaldt.logic.propositional.proposition.api.Proposition.variable;
@@ -17,33 +15,33 @@ public class ResolutionPropositionProver implements PropositionProver {
     @Override
     public boolean prove(Proposition proposition) {
         final AndProposition cnf = (AndProposition) not(proposition).toCNF();
-        final List<List<Proposition>> resolvents = toResolvents(cnf);
+        final List<Set<Proposition>> resolvents = toResolvents(cnf);
         return resolve(resolvents);
     }
 
-    private List<List<Proposition>> toResolvents(AndProposition andProposition) {
-        List<List<Proposition>> resolvents = new ArrayList<>();
+    private List<Set<Proposition>> toResolvents(AndProposition andProposition) {
+        List<Set<Proposition>> resolvents = new ArrayList<>();
         final List<Proposition> disjunctions = andProposition.getPropositions();
         for (Proposition proposition : disjunctions) {
             final OrProposition orProposition = (OrProposition) proposition;
             final List<Proposition> propositions = orProposition.getPropositions();
-            resolvents.add(new ArrayList<>(propositions));
+            resolvents.add(new HashSet<>(propositions));
         }
         return resolvents;
     }
 
-    private boolean resolve(List<List<Proposition>> resolvents) {
+    private boolean resolve(List<Set<Proposition>> resolvents) {
         for (int leftIndex = 0; leftIndex < resolvents.size(); leftIndex++) {
-            final List<Proposition> leftResolvent = resolvents.get(leftIndex);
+            final Set<Proposition> leftResolvent = resolvents.get(leftIndex);
             for (int rightIndex = leftIndex + 1; rightIndex < resolvents.size(); rightIndex++) {
-                final List<Proposition> rightResolvent = resolvents.get(rightIndex);
-                final List<Proposition> newResolvent = resolve(leftResolvent, rightResolvent);
+                final Set<Proposition> rightResolvent = resolvents.get(rightIndex);
+                final Set<Proposition> newResolvent = resolve(leftResolvent, rightResolvent);
                 if (newResolvent != null) {
                     if (newResolvent.isEmpty()) {
                         return true;
                     } else if(!resolvents.contains(newResolvent)) {
                         resolvents.add(newResolvent);
-                        resolvents.sort(Comparator.comparing(List::size));
+                        resolvents.sort(Comparator.comparing(Set::size));
                         return resolve(resolvents);
                     }
                 }
@@ -52,7 +50,7 @@ public class ResolutionPropositionProver implements PropositionProver {
         return false;
     }
 
-    private List<Proposition> resolve(List<Proposition> leftResolvent, List<Proposition> rightResolvent) {
+    private Set<Proposition> resolve(Set<Proposition> leftResolvent, Set<Proposition> rightResolvent) {
         String resolvableVariableName = null;
         for (Proposition proposition : leftResolvent) {
             final String variableName = getVariableName(proposition);
@@ -79,15 +77,14 @@ public class ResolutionPropositionProver implements PropositionProver {
         return proposition.findVariableNames().stream().findFirst().get();
     }
 
-    private List<Proposition> createNewResolvent(
-            List<Proposition> leftResolvent, List<Proposition> rightResolvent, String resolvableVariableName) {
-        final List<Proposition> newResolvent = new ArrayList<>();
+    private Set<Proposition> createNewResolvent(
+            Set<Proposition> leftResolvent, Set<Proposition> rightResolvent, String resolvableVariableName) {
+        final Set<Proposition> newResolvent = new HashSet<>();
         newResolvent.addAll(leftResolvent);
         newResolvent.addAll(rightResolvent);
         final Proposition resolvableVariable = variable(resolvableVariableName);
         newResolvent.remove(resolvableVariable);
         newResolvent.remove(not(resolvableVariable));
-        newResolvent.sort(Comparator.comparing(this::getVariableName));
         return newResolvent;
     }
 
